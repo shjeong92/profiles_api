33926bc6498c81ae0f5e6b775e23c86733af145b
Sanghyuk Jeong <shjeong920522@gmail.com>
2021-04-17 14:42:19 +0900

    Add login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+#query set이 제공되기때문에 basename이 필요가없당
 router.register('profile', views.UserProfileViewSet)
-
 urlpatterns = [
     path('hello-view', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),  
     path('', include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 from rest_framework import views, authentication, filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
@@ -111,3 +113,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
